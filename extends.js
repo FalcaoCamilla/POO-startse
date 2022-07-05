b9d5459189cac4868363f965627e1aa2eea4acc2
camilla.falcao <millafalcao.acad@gmail.com>
2022-07-04 23:17:37 -0300

    extends

--- extends.js
+++ extends.js
@@ -1,33 +1,44 @@
 'use strict';
 
 class Produto{
-    tipo;
-    constructor(tipo){
-        this.tipo = tipo;
+    #tipo;
+    #valor;
+    constructor(tipo, valor){
+        this.#validaValor(valor);
+        this.#tipo = tipo;
+        this.#valor = valor;
+    }
+    #validaValor(valor){
+        if (typeof(valor) !== 'number'){
+            console.log('Valor inválido!');
+        }
     }
     getDados(){
         return `
-        Tipo: ${this.tipo}`;
+        Tipo: ${this.#tipo}
+        Valor: ${this.#valor}`;
     }
 }
 
 class Livro extends Produto{
-    titulo;
-    numPag;
-    constructor(titulo, numPag){
-        super('Livro');
-        this.titulo = titulo;
-        this.numPag = numPag;
-        console.log(super.getDados());
+    #titulo;
+    #numPag;
+    constructor(titulo, numPag, valor){
+        super('Livro', valor);
+        this.#titulo = titulo;
+        this.#numPag = numPag;
+        //console.log(super.getDados());
     }
     exibeDadosLivro(){
-        console.log(`${this.getDados()}. ${this.titulo} - ${this.numPag}`);
+        console.log(`${this.getDados()}. ${this.#titulo} - ${this.#numPag}`);
     }
 }
 
-//const prod = new Produto('Produto genérico')
-//console.log(prod.getDados());
+const prod = new Produto('Produto genérico', 50)
+console.log(prod.getDados()); //Tipo: Produto genérico Valor: 50
 
-const liv = new Livro('Meu Livro', 122)
-//console.log(liv);
-liv.exibeDadosLivro();
+const liv = new Livro('Meu Livro', 122, 30)
+console.log(liv); //Livro {}
+liv.exibeDadosLivro(); //Tipo: Livro Valor: 30. Meu Livro - 122
+
+//atributos privados só podem ser acessados através de metódos públicos que os retornem
